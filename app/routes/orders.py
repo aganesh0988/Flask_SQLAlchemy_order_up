e69866bad02f1ca11197050255233b28ab7a4723
Andrea <aganesh0988@gmail.com>
2020-10-14 16:10:52 -0500

    added login_required

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
+from flask_login import login_required
 
 bp = Blueprint("orders", __name__, url_prefix="")
 
 
 @bp.route("/")
+@login_required
 def index():
     return "Order Up!"
