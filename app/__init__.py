be79489835f9417e8924dd00e1ea7a85f5dd66d6
Andrea <aganesh0988@gmail.com>
2020-10-14 15:24:44 -0500

    integrated flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from .config import Configuration
-from .models import db
+from .models import db, Employee
+from flask_login import LoginManager
 from .routes import orders
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(orders.bp)
 db.init_app(app)
+
+login = LoginManager(app)
+login.login_view = "session.login"
+
+
+@login.user_loader
+def load_user(id):
+    return Employee.query.get(int(id))
